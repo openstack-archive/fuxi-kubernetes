abc4a65b5fa053e989b529ff76b870f6ec5ebba2
zengchen <chenzeng2@huawei.com>
2017-06-23 16:34:22 +0800

    Add Cinder client
    Change-Id: Ie5b39e2f48715df8c226c8b28faba6d3b909b033
    reference: https://review.openstack.org/#/c/474038/
    blueprint:implement-flex-volume-driver

--- fuxi_kubernetes/common/config.py
+++ fuxi_kubernetes/common/config.py
@@ -10,6 +10,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from kuryr.lib import config as kuryr_config
+import os
 from oslo_config import cfg
 from oslo_log import log as logging
 
@@ -37,9 +39,25 @@ flexvolume_driver_opts = [
 ]
 
 
+cinder_group = cfg.OptGroup(
+    'cinder',
+    title='Cinder Options',
+    help=_('Configuration options for OpenStack Cinder'))
+
+
+cinder_opts = [
+    cfg.StrOpt('region_name',
+               default=os.environ.get('REGION'),
+               help=_('Region name of this node. This is used when picking'
+                      ' the URL in the service catalog.')),
+]
+
+
 CONF = cfg.CONF
 logging.register_options(CONF)
 CONF.register_opts(flexvolume_driver_opts, flexvolume_driver_group.name)
+CONF.register_opts(cinder_opts, group=cinder_group.name)
+kuryr_config.register_keystoneauth_opts(CONF, cinder_group.name)
 
 
 def init(args, **kwargs):
